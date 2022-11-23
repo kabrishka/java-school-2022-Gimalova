0857defe6bbc61438171409d7c0dd728ca7aa1ea
kabrishka <99499702+kabrishka@users.noreply.github.com>
2022-11-23 17:13:40 +0300

    Create DataBank.java

---
+++ src/ru/croc/task13/DataBank.java
@@ -0,0 +1,14 @@
+package ru.croc.task13;
+
+import java.util.*;
+
+public class DataBank {
+    private Map<Integer, String> films;
+    private Map<Integer, List<Integer>> history;
+
+    public DataBank(String filmsPath, String historyPath) {
+        films = Converter.getFilms(filmsPath);
+        history = Converter.getHistory(historyPath);
+    }
+
+}
