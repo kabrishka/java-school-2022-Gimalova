06e1a2900c60570036f6f9b0e2e872aa4b7bbfb3
kabrishka <sharipovalliss@gmail.com>
2022-11-23 17:11:29 +0300

    Создан класс конвертер, класс содержащий информацию по фильмам и пользователям + их истории

---
+++ src/ru/croc/task13/Task13.java
@@ -0,0 +1,13 @@
+package ru.croc.task13;
+
+import java.util.*;
+
+public class Task13 {
+
+    public static void main(String[] args) {
+        String path1 = "/Users/karinagimalova/IdeaProjects/CROC_task_1/src/ru/croc/task13/res/films.txt";
+        String path2 = "/Users/karinagimalova/IdeaProjects/CROC_task_1/src/ru/croc/task13/res/history.txt";
+
+
+    }
+}
