bd45dfd4260ad7fdaf8a09d256cdcb7a1fd40c58
kabrishka <sharipovalliss@gmail.com>
2022-11-26 03:03:11 +0300

    finish task13

--- src/ru/croc/task13/Converter.java
+++ src/ru/croc/task13/Converter.java
@@ -11,20 +11,13 @@ public class Converter {
      * @return films словарь id и названия фильма
      */
     public static HashMap<Integer, String> getFilms(String path) {
-<<<<<<< HEAD
 
         HashMap<Integer, String> films = new HashMap<>();
 
         try (BufferedReader in = new BufferedReader(new FileReader(path))) {
             String line;
-            while ((line = in.readLine()) != null) {;
-=======
-        HashMap<Integer, String> films = new HashMap<>();
+            while ((line = in.readLine()) != null) {
 
-        try (BufferedReader in = new BufferedReader(new FileReader(path))) {
-            String line = in.readLine();
-            while (line != null) {;
->>>>>>> origin/master
                 Integer id = Integer.parseInt(line.substring(0, line.indexOf(',')));
                 String name = line.substring(line.indexOf(',') + 1);
                 films.put(id, name);
@@ -44,20 +37,13 @@ public class Converter {
      */
 
     public static Map<Integer, List<Integer>> getHistory(String path) throws NumberFormatException {
-<<<<<<< HEAD
 
         Map<Integer, List<Integer>> history = new HashMap<>();
         int userId = 0;
         try (BufferedReader r = new BufferedReader(new FileReader(path))) {
             String line;
             while ((line = r.readLine()) != null) {
-=======
-        Map<Integer, List<Integer>> history = new HashMap<>();
-        int userId = 0;
-        try (BufferedReader r = new BufferedReader(new FileReader(path))) {
-            String line = r.readLine();
-            while (line != null) {
->>>>>>> origin/master
+
                 String[] films = line.split(",");
 
                 //добавление фильма в список фильмоа
@@ -71,10 +57,7 @@ public class Converter {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-<<<<<<< HEAD
 
-=======
->>>>>>> origin/master
         return history;
     }
 }
