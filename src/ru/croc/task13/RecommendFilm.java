9ed8af662b25528b2f8cf1d1998240e6592cbaa3
kabrishka <sharipovalliss@gmail.com>
2022-12-02 00:24:47 +0300

    did task 13

--- src/ru/croc/task13/RecommendFilm.java
+++ src/ru/croc/task13/RecommendFilm.java
@@ -1,6 +1,7 @@
 package ru.croc.task13;
 
 import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class RecommendFilm {
@@ -10,12 +11,14 @@ public class RecommendFilm {
         this.bank = new DataBank(filmPath, historyPath);
     }
 
-    //выбираются все пользователи, которые посмотрели
-    //минимум половину фильмов пользователя, для которого формируется рекомендация
-    //Из отобранных списков исключаются все, которые пользователь уже посмотрел.
-    //Для оставшегося списка фильмов подсчитывается суммарное количество просмотров
-    //среди всех пользователей сервиса и фильм с максимальным числом просмотров выбирается
-    //как рекомендация (если таких фильмов оказалось несколько, выбирается любой из них).
+    /*
+    * выбираются все пользователи, которые посмотрели
+    * минимум половину фильмов пользователя, для которого формируется рекомендация
+    * Из отобранных списков исключаются все, которые пользователь уже посмотрел.
+    * Для оставшегося списка фильмов подсчитывается суммарное количество просмотров
+    * среди всех пользователей сервиса и фильм с максимальным числом просмотров выбирается
+    * как рекомендация (если таких фильмов оказалось несколько, выбирается любой из них).
+    * */
     public String recommendedFilm(String userFilms) {
         //получаем список (неуппорядоченный!) всех фильмов, которые можно предложить
         List<Integer> listSuitableFilms = getGoodFilms(userFilms,bank.getHistory());
@@ -23,19 +26,28 @@ public class RecommendFilm {
     }
 
     private Integer getKeyPopularFilm(List<Integer> listSuitableFilms) {
-        //часто встречающиеся фильмы
-        Map<Integer, Integer> frequentlySeenFilms = new HashMap<>();
-        for (Integer key : listSuitableFilms) {
-            if (!frequentlySeenFilms.containsKey(key)) {
-                frequentlySeenFilms.put(key, 1);
-            } else {
-                Integer value = frequentlySeenFilms.get(key);
-                frequentlySeenFilms.put(key, value + 1);
-            }
-        }
+//        Map<Integer, Integer> frequentlySeenFilms = new HashMap<>();
+//
+//        listSuitableFilms
+//                .forEach(film -> {
+//                    if(!frequentlySeenFilms.containsKey(film)){
+//                        frequentlySeenFilms.put(film, Collections.frequency(listSuitableFilms, film));
+//                        System.out.println("Key: " + film + " Value: " + frequentlySeenFilms.get(film));
+//                    }
+//                });
 
-        return frequentlySeenFilms.entrySet().stream().max((entry1, entry2) -> entry1.getValue() > entry2.getValue() ? 1 : -1).get().getKey();
+        Map<Integer, Long> frequentlySeenFilms = listSuitableFilms.stream()
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
 
+
+        Integer keyPopularFilm = frequentlySeenFilms
+                .entrySet()
+                .stream()
+                .max((entry1, entry2) -> entry1.getValue() > entry2.getValue() ? 1 : -1)
+                .get()
+                .getKey();
+
+        return keyPopularFilm;
     }
 
     /*
@@ -46,59 +58,38 @@ public class RecommendFilm {
     * @param userFilms строка фильмов пользователя
     * @param history история просмотров других пользователей
     * */
-    private List<Integer> getGoodFilms(String userFilms, Map<Integer, List<Integer>> history){
-        //преобразование строки к массиву
-        List<String> list = Arrays.asList(userFilms.split(","));
-        //Конвертируем List<String> к Stream<String> с использованием List.stream().
-        //Конвертируем Stream<String> к Stream<Integer> с использованием Stream.map().
-        //Accumulate Stream<Integer> в List<Integer> с использованием Collectors.toList().
-        //преобразование list к set , чтобы исключить повторения
-        Set<Integer> uniqueUserFilms = Set.copyOf(list.stream()
+    private List<Integer> getGoodFilms(String userFilms, Map<Integer, List<Integer>> history) {
+        Set<Integer> uniqueUserFilms = Arrays.stream(userFilms.split(","))
                 .map(Integer::parseInt)
-                .collect(Collectors.toList()));
+                .collect(Collectors.toSet());
 
         //нам необходимы НЕ уникальные значения, чтобы пройтись по массиву
         //и посчитать кол-во повторений и вывести рекомендацию
         List<Integer> listSuitableFilms = new ArrayList<>();
 
+
         List<Integer> keys = new ArrayList<>(history.keySet());
         //берем по id
         for (Integer key : keys) {
-            int count = 0;
-//            //для того, чтобы не учитывать один и тот же фильм, приведем list фильмов к set
+            int count = 0;//просмотренно одинаковых фильмов
+
             List<Integer> historyFilmsList = history.get(key);
 
             //временная переменная
             List<Integer> recommendFilms = new ArrayList<>();
 
-            Iterator<Integer> it = historyFilmsList.iterator();
-
-            while (it.hasNext()) {
-                Integer film = it.next();
-
+            for (Integer film : historyFilmsList) {
                 if (uniqueUserFilms.contains(film)) {
                     count++;
-                    historyFilmsList = deleteItem(historyFilmsList, film); //удаление всех эелементов списка == film
-                    it = historyFilmsList.iterator();//необходимо обновить итератор, тк список изменился
-
                 } else {
                     recommendFilms.add(film);
                 }
             }
             if (count >= Math.round(uniqueUserFilms.size() / 2)) {
                 listSuitableFilms.addAll(recommendFilms);
-
             }
+
         }
         return listSuitableFilms;
-    }
-
-    private List<Integer> deleteItem(List<Integer> mutableArray, Integer delValue) {
-        List<Integer> newArray = new ArrayList<>();
-        for (Integer currValue : mutableArray) {
-            if (!(currValue.compareTo(delValue) == 0)) newArray.add(currValue);
-        }
-
-        return newArray;
     }
 }
--- src/ru/croc/task13/Task13.java
+++ src/ru/croc/task13/Task13.java
@@ -3,8 +3,8 @@ package ru.croc.task13;
 public class Task13 {
 
     public static void main(String[] args) {
-        String filmPath = "/Users/karinagimalova/IdeaProjects/CROC_task_1/src/ru/croc/task13/res/films.txt";
-        String historyPath = "/Users/karinagimalova/IdeaProjects/CROC_task_1/src/ru/croc/task13/res/history.txt";
+        String filmPath = "src/ru/croc/task13/res/films.txt";
+        String historyPath = "src/ru/croc/task13/res/history.txt";
 
         String films = "2,4";
 
